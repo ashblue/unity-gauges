4fe2be1b81ff9f4d34360b2310fd3cc21503fe7b
Ash Blue <ash@blueashes.com>
2021-04-07 23:05:03 -0600

    ci: attempting nightly build fix

--- publish-nightly.sh
+++ publish-nightly.sh
@@ -7,7 +7,7 @@ setup_git() {
 
 update_nightly_branch() {
     printf "Run nightly update \n"
-    git remote add origin-nightly https://${{env.GH_TOKEN}}@github.com/ashblue/unity-gauges.git
+    git remote add origin-nightly "https://${env.GH_TOKEN}@github.com/ashblue/unity-gauges.git"
     git subtree split --prefix Assets/com.fluid.unity-gauges -b nightly
     git push -f origin-nightly nightly:nightly
 }
