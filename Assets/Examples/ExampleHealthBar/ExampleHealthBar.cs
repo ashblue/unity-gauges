8555dd24c95b278b32184b13f06a0b3ee0f87f61
Ash Blue <ash@blueashes.com>
2021-04-07 21:08:56 -0600

    docs(readme.md): added usage example and intro

--- Assets/Examples/ExampleHealthBar/ExampleHealthBar.cs
+++ Assets/Examples/ExampleHealthBar/ExampleHealthBar.cs
@@ -1,24 +1,28 @@
+using CleverCrow.Fluid.Gauges;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
-namespace CleverCrow.Fluid.Gauges {
-    public class ExampleHealthBar : MonoBehaviour {
-        private readonly Gauge _health = new Gauge(10);
+public class ExampleHealthBar : MonoBehaviour {
+    private readonly Gauge _health = new Gauge(10);
 
-        [SerializeField]
-        private Slider _healthBar;
+    [SerializeField]
+    private Slider _healthBar;
 
-        private void Start () {
-            _healthBar.maxValue = _health.ChargeMax;
-            _healthBar.value = 5;
-        }
+    [SerializeField]
+    private UnityEvent _onChange;
 
-        public void AddHealth (int health) {
-            _healthBar.value += health;
-        }
+    private void Start () {
+        _healthBar.maxValue = _health.ChargeMax;
+        _healthBar.value = 5;
+        _healthBar.onValueChanged.AddListener((changeValue) => _onChange.Invoke());
+    }
 
-        public void RemoveHealth (int health) {
-            _healthBar.value -= health;
-        }
+    public void AddHealth (int health) {
+        _healthBar.value += health;
+    }
+
+    public void RemoveHealth (int health) {
+        _healthBar.value -= health;
     }
 }
